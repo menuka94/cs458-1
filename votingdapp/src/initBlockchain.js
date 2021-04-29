940c963fc8b71d998efaed01c6e794b1f5770efa
Menuka Warushavithana <menuka.warushavithana@gmail.com>
2021-04-28 20:58:38 -0600

    feat(DAPP): add missing react dependencies, use PollFactory ABI

--- votingdapp/src/initBlockchain.js
+++ votingdapp/src/initBlockchain.js
@@ -1,11 +1,12 @@
 // symlink Voting.json to: ../../artifacts/contracts/Voting.sol/Voting.json
-import VotingContract from "./Voting.json";
+// import VotingContract from "./Voting.json";
+import PollFactory from "./contract_ABI/PollFactory.sol/PollFactory.json"
 import {ethers} from "ethers";
 import store from "./redux/store";
 
-function huh(data) {
+function blockchainInitialized(data) {
   return {
-    type: "SOMETHING",
+    type: "BLOCKCHAIN_INITIALIZED",
     payload: data
   };
 }
@@ -21,11 +22,11 @@ const initBlockchain = async () => {
   const userAddress = await signer.getAddress();
   console.log("userAddress:", userAddress);
 
-  console.log(VotingContract);
-  //const parsed = JSON.parse(VotingContract);
-  let contract = new ethers.Contract('0x5FbDB2315678afecb367f032d93F642f64180aa3', VotingContract.abi, signer);
+  console.log(PollFactory);
+  const parsedAbi = JSON.parse(PollFactory.abi);
+  let contract = new ethers.Contract('0x5FbDB2315678afecb367f032d93F642f64180aa3', parsedAbi, signer);
   let data = {provider, signer, contract, userAddress};
-  store.dispatch(huh(data));
+  store.dispatch(blockchainInitialized(data));
   console.log("initBlockchain end");
   return data;
 }
