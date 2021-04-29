6604c03e16ae7d53cc5835f0425c99c4d2610657
Menuka Warushavithana <menuka.warushavithana@gmail.com>
2021-04-28 21:44:55 -0600

    feat(build): add Makefile, update .gitignore

---
+++ Makefile
@@ -0,0 +1,9 @@
+build:
+	npx hardhat compile
+
+clean:
+	npx hardhat clean
+
+update-abi:
+	rm -rf ./votingdapp/src/contracts_abi/PollFactory.sol;
+	cp -r ./artifacts/contracts/PollFactory.sol ./votingdapp/src/contracts_abi/
--- votingdapp/src/pages/MainPage.js
+++ votingdapp/src/pages/MainPage.js
@@ -8,7 +8,7 @@ import Test from "../test";
 import store from "../redux/store";
 
 function mapStateToProps(state) {
-	console.log("Mainpage mapStateToProps", state);
+	console.log("MainPage mapStateToProps", state);
 	return state;
 }
 
@@ -18,11 +18,11 @@ class MainPage extends Component {
 		//this.state = {isConnected: false, peers: 0, version: '', contract: null, numpolls: 0, polls: []};
 		//console.log("componentDidMount: contract", this.state.contract);
 		this.state = store.getState();
-		console.log("Mainpage constructor state:", this.state);
+		console.log("MainPage constructor state:", this.state);
 	}
 	componentDidMount = async() => {
 	try {
-		console.log("Mainpage componentDidMount store:", store.getState());
+		console.log("MainPage componentDidMount store:", store.getState());
 		//this.state = store.getState();
 	} catch (error) {
 		alert("Failed to load provider");
@@ -34,11 +34,11 @@ class MainPage extends Component {
 
 	//<Test  contract={this.state} />
 	render() {
-		console.log("Mainpage render props:", this.props);
-		console.log("Mainpage render getState:", store.getState());
-		console.log("Mainpage render this.state.polls:", this.state.polls);
-		console.log("Mainpage render this.props.polls:", this.props.polls);
-		console.log("Mainpage render this.state.numpolls:", this.state.numpolls);
+		console.log("MainPage render props:", this.props);
+		console.log("MainPage render getState:", store.getState());
+		console.log("MainPage render this.state.polls:", this.state.polls);
+		console.log("MainPage render this.props.polls:", this.props.polls);
+		console.log("MainPage render this.state.numpolls:", this.state.numpolls);
 		return (
 			<div>
 			<h2>Connected?:</h2><br/>
