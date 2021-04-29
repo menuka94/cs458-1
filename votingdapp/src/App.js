9d5d68ade03e96dc8af573661b311b00e96f9e43
Menuka Warushavithana <menuka.warushavithana@gmail.com>
2021-04-29 16:40:56 -0600

    feat: add react-bootstrap

--- votingdapp/src/App.js
+++ votingdapp/src/App.js
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import React, {Component} from "react";
 //import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
@@ -10,101 +11,106 @@ import ShowPoll from "./pages/ShowPoll"
 import MainPage from "./pages/MainPage";
 
 import store from "./redux/store";
+import {Container} from "react-bootstrap";
+import TopBar from "./components/TopBar";
 
 function initState(data) {
-  return {
-    type: "setpolls",
-    payload: data
-  };
+    return {
+        type: "setpolls",
+        payload: data
+    };
 }
 
 function setnumpolls(data) {
-  return {
-    type: "setnumpolls",
-    payload: data
-  };
+    return {
+        type: "setnumpolls",
+        payload: data
+    };
 }
 
 function setpolllist(data) {
-  return {
-    type: "setpolllist",
-    payload: data
-  };
+    return {
+        type: "setpolllist",
+        payload: data
+    };
 }
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    console.log("App constructor");
-    this.state = store.getState();
-    //store.dispatch(initState(null));
-  }
-
-  componentDidMount = async () => {
-    try {
-      console.log("App componentDidMount");
-      const poll = await initBlockchain();
-      const nump = await poll.contract.numPolls();
-      console.log("App componentDidMount nump:", nump.toNumber());
-      //this.setState({numpolls: nump.toNumber()});
-      store.dispatch(setnumpolls({numpolls: nump.toNumber()}));
-      //this.setState({contract: poll.voting});
-      let i;
-      for (i = 0; i < nump.toNumber(); i++) {
-        this.state.polls.push(i + 1);
-      }
-      store.dispatch(setpolllist({polls: this.state.polls}));
-      // weirdly I need to do this here for Mainpage to update
-      // regardless of whether mainpage uses this.props.polls
-      // or this.state.polls
-      this.setState({polls: this.state.polls});
-      console.log("App componentDidMount polls:", this.state.polls);
-      console.log("App componentDidMount state:", this.state);
-      console.log("App componentDidMount store getState:", store.getState());
-      this.state = store.getState();
-      console.log("App componentDidMount end");
-    } catch (error) {
-      alert("Failed to load provider");
-      console.log(error);
+    constructor(props) {
+        super(props);
+        console.log("App constructor");
+        this.state = store.getState();
+        //store.dispatch(initState(null));
     }
-  };
+
+    componentDidMount = async () => {
+        try {
+            console.log("App componentDidMount");
+            const poll = await initBlockchain();
+            const nump = await poll.contract.numPolls();
+            console.log("App componentDidMount nump:", nump.toNumber());
+            //this.setState({numpolls: nump.toNumber()});
+            store.dispatch(setnumpolls({numpolls: nump.toNumber()}));
+            //this.setState({contract: poll.voting});
+            let i;
+            for (i = 0; i < nump.toNumber(); i++) {
+                this.state.polls.push(i + 1);
+            }
+            store.dispatch(setpolllist({polls: this.state.polls}));
+            // weirdly I need to do this here for Mainpage to update
+            // regardless of whether mainpage uses this.props.polls
+            // or this.state.polls
+            this.setState({polls: this.state.polls});
+            console.log("App componentDidMount polls:", this.state.polls);
+            console.log("App componentDidMount state:", this.state);
+            console.log("App componentDidMount store getState:", store.getState());
+            this.state = store.getState();
+            console.log("App componentDidMount end");
+        } catch (error) {
+            alert("Failed to load provider");
+            console.log(error);
+        }
+    };
 
 
-  render() {
-    return (
-      <Provider store={store}>
+    render() {
+        return (
+          <Provider store={store}>
+              <Router>
+                  <Container>
+                      <TopBar state={this.state}/>
+                      <Route exact path="/" component={MainPage}/>
+                      <Route exact path="/ShowPoll" component={ShowPoll}/>
+                  </Container>
+              </Router>
+          </Provider>
+        );
+    }
+
+    /*
+    render() {
+      return (
         <Router>
-          <Route exact path="/" component={MainPage}/>
-          <Route exact path="/ShowPoll" component={ShowPoll}/>
+        <Route exact path ="/" component={ShowPoll}/>
+        <div>
+        <h2>Connected?:</h2><br/>
+        {this.state.isConnected?'Connected':'Not connected'}
+        <br/>
+        <h2>Node info:</h2><br /> {this.state.version}
+        <Test  contract={this.state} />
+        <h2>Numpolls: {this.state.numpolls}</h2><br/>
+        <h2>isConnected: {this.state.isConnected}</h2><br/>
+        <ul>
+        {this.state.polls.map((thing) => (
+          <li key={thing}>{thing}</li>
+        ))}
+        </ul>
+        <Link to="/ShowPoll">ShowPoll</Link>
+        </div>
         </Router>
-      </Provider>
-    );
-  }
-
-  /*
-  render() {
-    return (
-      <Router>
-      <Route exact path ="/" component={ShowPoll}/>
-      <div>
-      <h2>Connected?:</h2><br/>
-      {this.state.isConnected?'Connected':'Not connected'}
-      <br/>
-      <h2>Node info:</h2><br /> {this.state.version}
-      <Test  contract={this.state} />
-      <h2>Numpolls: {this.state.numpolls}</h2><br/>
-      <h2>isConnected: {this.state.isConnected}</h2><br/>
-      <ul>
-      {this.state.polls.map((thing) => (
-        <li key={thing}>{thing}</li>
-      ))}
-      </ul>
-      <Link to="/ShowPoll">ShowPoll</Link>
-      </div>
-      </Router>
-    )
-  }
-  */
+      )
+    }
+    */
 }
 
 export default App;
---
+++ votingdapp/src/components/TopBar.js
@@ -0,0 +1,31 @@
+import React, {Component} from "react";
+
+import {Menu, Header, Button, Navbar} from 'react-bootstrap';
+import {Link} from "react-router-dom";
+import {connect} from "react-redux";
+
+function mapStateToProps(state) {
+    return {
+        userAddress: state.userAddress
+    }
+}
+
+class TopBar extends Component {
+    render() {
+        return (
+          <div>
+              <Navbar>
+                  <Navbar.Brand href="#home">Completely Fair Voting DApp</Navbar.Brand>
+                  <Navbar.Toggle/>
+                  <Navbar.Collapse className="justify-content-end">
+                      <Navbar.Text>
+                          Your account address: <a href="">{this.props.userAddress}</a>
+                      </Navbar.Text>
+                  </Navbar.Collapse>
+              </Navbar>
+          </div>
+        );
+    }
+}
+
+export default connect(mapStateToProps)(TopBar);
